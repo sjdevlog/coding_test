fcbeea35fe192d927d132e5522a25e1401f8f208
Sujung Park <126868619+sjdevlog@users.noreply.github.com>
2026-03-03 15:32:50 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length];
+
+        for (int i = 0; i < num_list.length; i++) {
+            answer[i] = num_list[(i + n) % num_list.length];
+        }
+
+        return answer;
+    }
+}
