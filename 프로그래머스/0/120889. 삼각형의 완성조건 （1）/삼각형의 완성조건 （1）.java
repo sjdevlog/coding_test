89575e933fdcd4eee37230498b89894db4441263
Sujung Park <126868619+sjdevlog@users.noreply.github.com>
2026-03-03 16:08:47 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.42 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] sides) {
+        Arrays.sort(sides);
+        
+        int a = sides[0];
+        int b = sides[1];
+        int c = sides[2];
+        
+        if (a + b > c) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+}
