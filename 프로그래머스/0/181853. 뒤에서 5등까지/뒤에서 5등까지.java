bc1da02e61abede422c12c5aa100d337e904020d
Sujung Park <126868619+sjdevlog@users.noreply.github.com>
2026-03-27 20:45:04 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 1.09 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        Arrays.sort(num_list);
+        int[] arr = new int[5];
+        for (int i = 0; i < 5; i++) {
+            arr[i] = num_list[i];
+        }
+        return arr;
+    }
+}
