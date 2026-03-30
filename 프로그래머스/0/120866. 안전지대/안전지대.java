eccc518e0c7f1d48509e647c26b190d8d0e4b4d8
Sujung Park <126868619+sjdevlog@users.noreply.github.com>
2026-03-30 20:02:42 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int solution(int[][] board) {
+        int n = board.length;
+        int[][] danger = new int[n][n];
+        
+        int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 1) {
+                    danger[i][j] = 1; // 지뢰 칸도 위험
+                    
+                    for (int k = 0; k < 8; k++) {
+                        int ni = i + dx[k];
+                        int nj = j + dy[k];
+                        
+                        if (ni >= 0 && ni < n && nj >= 0 && nj < n) {
+                            danger[ni][nj] = 1;
+                        }
+                    }
+                }
+            }
+        }
+        
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (danger[i][j] == 0) {
+                    answer++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
